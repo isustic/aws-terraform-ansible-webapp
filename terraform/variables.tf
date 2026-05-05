9adda02fa2b869551d44a0c31542f6380804d250
Ionut <isustic@Ionuts-MacBook-Pro.local>
2026-05-05 19:24:06 +0300

    Add Terraform project variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,20 @@ variable "aws_region" {
   type        = string
   default     = "eu-central-1"
 }
+
+variable "project_name" {
+  description = "Name used to tag and identify project resources."
+  type        = string
+  default     = "aws-terraform-ansible-webapp"
+}
+
+variable "environment" {
+  description = "Deployment environment name."
+  type        = string
+  default     = "dev"
+}
+
+variable "allowed_ssh_cidr" {
+  description = "CIDR block allowed to SSH into the EC2 instance. Use your public IP with /32"
+  type        = string
+}
