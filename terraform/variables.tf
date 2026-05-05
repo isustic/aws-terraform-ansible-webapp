b58ee3c39c1e575b37365273aba0595f08873c2d
Ionut S. <ionut.sustic@gmail.com>
2026-05-05 21:07:36 +0300

    Add EC2 instance variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -20,3 +20,14 @@ variable "allowed_ssh_cidr" {
   description = "CIDR block allowed to SSH into the EC2 instance. Use your public IP with /32"
   type        = string
 }
+
+variable "instance_type" {
+  description = "EC2 instance type for the web server."
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "key_name" {
+  description = "Name of the AWS EC2 key pair used for SSH access."
+  type        = string
+}
